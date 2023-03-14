4c4f7f695bdad01ec1720e6b3a81b61a7c09043d
zain141 <zain.yasin141@gmail.com>
2023-03-14 11:09:21 +0500

    Revert "bnx2x: Fix VF's VLAN reconfiguration in reload."
    This reverts commit 7758bc95d4f6124770a9576a09cf92906b709205.

--- drivers/net/ethernet/broadcom/bnx2x/bnx2x_main.c
+++ drivers/net/ethernet/broadcom/bnx2x/bnx2x_main.c
@@ -8488,21 +8488,11 @@ int bnx2x_set_vlan_one(struct bnx2x *bp, u16 vlan,
 	return rc;
 }
 
-void bnx2x_clear_vlan_info(struct bnx2x *bp)
-{
-	struct bnx2x_vlan_entry *vlan;
-
-	/* Mark that hw forgot all entries */
-	list_for_each_entry(vlan, &bp->vlan_reg, link)
-		vlan->hw = false;
-
-	bp->vlan_cnt = 0;
-}
-
 static int bnx2x_del_all_vlans(struct bnx2x *bp)
 {
 	struct bnx2x_vlan_mac_obj *vlan_obj = &bp->sp_objs[0].vlan_obj;
 	unsigned long ramrod_flags = 0, vlan_flags = 0;
+	struct bnx2x_vlan_entry *vlan;
 	int rc;
 
 	__set_bit(RAMROD_COMP_WAIT, &ramrod_flags);
@@ -8511,7 +8501,10 @@ static int bnx2x_del_all_vlans(struct bnx2x *bp)
 	if (rc)
 		return rc;
 
-	bnx2x_clear_vlan_info(bp);
+	/* Mark that hw forgot all entries */
+	list_for_each_entry(vlan, &bp->vlan_reg, link)
+		vlan->hw = false;
+	bp->vlan_cnt = 0;
 
 	return 0;
 }
